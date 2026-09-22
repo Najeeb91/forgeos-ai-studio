44bd872f2f1aad451d51f440cbc35f729214e18c
Najeeb91 <k.najeeb92@gmail.com>
2026-09-22 13:22:46 +0530

    feat: connect ForgeOS control plane to Postgres

---
+++ src/lib/forge/use-project.ts
@@ -0,0 +1,3 @@
+import { getRouteApi } from "@tanstack/react-router";
+const projectRoute = getRouteApi("/projects/$slug");
+export function useForgeProject() { return projectRoute.useLoaderData().project; }
