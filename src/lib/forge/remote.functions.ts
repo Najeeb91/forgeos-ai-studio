06c4d8cef6bbdd2f3cb94f9116cbeebdcec941a7
Najeeb91 <k.najeeb92@gmail.com>
2026-09-24 04:01:33 +0530

    feat: connect project creation to worker

--- src/lib/forge/remote.functions.ts
+++ src/lib/forge/remote.functions.ts
@@ -1,6 +1,6 @@
 import { createServerFn } from "@tanstack/react-start";
 import { projects as seedProjects } from "./data";
-import { getProjectFromWorker } from "./worker-client";
+import { createForgeProject, getProjectFromWorker } from "./worker-client";
 import { listForgeProviders as listConfiguredProviders } from "./provider-routing";
 
 export const getForgeProject = createServerFn({ method: "GET" })
@@ -28,3 +28,7 @@ export const listForgeProviders = createServerFn({ method: "GET" }).handler(asyn
   providers: listConfiguredProviders(),
   source: "seed" as const,
 }));
+
+export const createForgeProjectRemote = createServerFn({ method: "POST" })
+  .validator((data: { slug: string; name: string; prompt: string }) => data)
+  .handler(async ({ data }) => createForgeProject(data));
